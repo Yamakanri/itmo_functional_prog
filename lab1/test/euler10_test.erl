1175e6a421a8653cc0a7ea21af892e099b1bdde5
Aleksandr Vasilev <118484231+Yamakanri@users.noreply.github.com>
2025-10-06 21:28:03 +0300

    Update euler10_test.erl

--- lab1/test/euler10_test.erl
+++ lab1/test/euler10_test.erl
@@ -7,7 +7,7 @@
 
 euler10_recursion_test() -> ?assertEqual(?ANSWER, recursion10:start()).
 
-euler10_tail_recursion_test() -> ?assertEqual(?ANSWER, accRecursion10:start()).
+euler10_tail_recursion_test() -> ?assertEqual(?ANSWER, acc_recursion10:start()).
 
 euler10_modular_test() -> ?assertEqual(?ANSWER, svertka10:start()).
 
