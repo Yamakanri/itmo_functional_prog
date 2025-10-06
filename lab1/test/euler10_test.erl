05c2cbd88956a612305b955c6e50668653bf22aa
Aleksandr Vasilev <118484231+Yamakanri@users.noreply.github.com>
2025-10-06 20:28:38 +0300

    Update euler10_test.erl

--- lab1/test/euler10_test.erl
+++ lab1/test/euler10_test.erl
@@ -1,1 +1,14 @@
+-module(euler10_test).
 
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+-define(ANSWER, 142913828922).
+
+euler10_recursion_test() -> ?assertEqual(?ANSWER, recursion10:start()).
+
+euler10_tail_recursion_test() -> ?assertEqual(?ANSWER, accRecursion10:start()).
+
+euler10_modular_test() -> ?assertEqual(?ANSWER, svertka10:start()).
+
+euler10_map_test() -> ?assertEqual(?ANSWER, otobraz10:start()).
