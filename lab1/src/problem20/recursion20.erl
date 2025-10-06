d2d652eadc8cc0b90b6631d688d92d7865a8e9f6
Aleksandr Vasilev <118484231+Yamakanri@users.noreply.github.com>
2025-10-06 19:41:29 +0300

    Create recursion20.erl

---
+++ lab1/src/problem20/recursion20.erl
@@ -0,0 +1,16 @@
+-module(recursion20).
+-export([start/0]).
+
+% Задание: Найти сумму всех цифр в числе 100!
+
+start() -> list_sum(list_splitter(factorial(100))).
+
+% Получение факториала переданного числа (100!)
+factorial(0) -> 1;
+factorial(N) when N>0 -> N* factorial(N-1).
+
+list_splitter(0) -> [];
+list_splitter(N) -> list_splitter(N div 10) ++ [N rem 10].
+
+list_sum([]) -> 0;
+list_sum([H|T]) -> list_sum(T) + H.
