9fb1949a8cbbfe5c9f47d9138b6eb54e6f472515
Aleksandr Vasilev <118484231+Yamakanri@users.noreply.github.com>
2025-10-06 21:23:23 +0300

    Update recursion20.erl

--- lab1/src/problem20/recursion20.erl
+++ lab1/src/problem20/recursion20.erl
@@ -7,10 +7,10 @@ start() -> list_sum(list_splitter(factorial(100))).
 
 % Получение факториала переданного числа (100!)
 factorial(0) -> 1;
-factorial(N) when N>0 -> N* factorial(N-1).
+factorial(N) when N>0 -> N * factorial(N - 1).
 
 list_splitter(0) -> [];
 list_splitter(N) -> list_splitter(N div 10) ++ [N rem 10].
 
 list_sum([]) -> 0;
-list_sum([H|T]) -> list_sum(T) + H.
+list_sum([H | T]) -> list_sum(T) + H.
