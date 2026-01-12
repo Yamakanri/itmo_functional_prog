865c6a251ec60c449f82f59fd081c18e67550743
Aleksandr Vasilev <118484231+Yamakanri@users.noreply.github.com>
2026-01-13 01:04:20 +0300

    Update run_tests.erl

--- lab3/scripts/run_tests.erl
+++ lab3/scripts/run_tests.erl
@@ -1,49 +1,44 @@
-%% run_tests.erl
-%% Модуль для запуска всех тестов
+%% lab3/scripts/run_tests.erl
 -module(run_tests).
--export([all/0]).
+-include_lib("eunit/include/eunit.hrl").
+-export([main/0]).
 
-all() ->
-  io:format("=== Компиляция исходников ===~n"),
-  %% Компиляция исходников
-  lists:foreach(fun(F) -> io:format("Компилируем ~s~n", [F]), compile:file(F) end,
-    ["proc/src/linear_interpolation.erl",
-      "proc/src/lab3_app.erl",
-      "proc/src/lagrange_interpolation.erl",
-      "proc/src/lab3_sup.erl",
-      "proc/src/main.erl",
-      "proc/src/interpolation.erl",
-      "proc/src/io_server.erl",
-      "gen_server/src/lab3_gen_server_sup.erl",
-      "gen_server/src/lab3_gen_server_app.erl",
-      "gen_server/src/lagrange_server.erl",
-      "gen_server/src/interpolation_sup.erl",
-      "gen_server/src/output_server.erl",
-      "gen_server/src/input_server.erl",
-      "gen_server/src/interpolation_server.erl",
-      "gen_server/src/linear_server.erl"]),
+main() ->
+    %% Список исходников
+    Src = [
+      "lab3/proc/src/linear_interpolation.erl",
+      "lab3/proc/src/lab3_app.erl",
+      "lab3/proc/src/lagrange_interpolation.erl",
+      "lab3/proc/src/lab3_sup.erl",
+      "lab3/proc/src/main.erl",
+      "lab3/proc/src/interpolation.erl",
+      "lab3/proc/src/io_server.erl",
+      "lab3/gen_server/src/lab3_gen_server_sup.erl",
+      "lab3/gen_server/src/lab3_gen_server_app.erl",
+      "lab3/gen_server/src/lagrange_server.erl",
+      "lab3/gen_server/src/interpolation_sup.erl",
+      "lab3/gen_server/src/output_server.erl",
+      "lab3/gen_server/src/input_server.erl",
+      "lab3/gen_server/src/interpolation_server.erl",
+      "lab3/gen_server/src/linear_server.erl"
+    ],
 
-  io:format("=== Компиляция тестов ===~n"),
-  %% Компиляция тестов
-  lists:foreach(fun(F) -> io:format("Компилируем тест ~s~n", [F]), compile:file(F) end,
-    ["proc/test/proc_linear_interpolation_test.erl",
-      "proc/test/proc_parser_test.erl",
-      "proc/test/proc_points_generator_test.erl",
-      "proc/test/proc_lagrange_interpolation_test.erl",
-      "gen_server/test/gen_server_interpolation_test.erl",
-      "gen_server/test/gen_server_parser_test.erl"]),
+    %% Список тестов
+    Tests = [
+      "lab3/proc/test/proc_linear_interpolation_test.erl",
+      "lab3/proc/test/proc_parser_test.erl",
+      "lab3/proc/test/proc_points_generator_test.erl",
+      "lab3/proc/test/proc_lagrange_interpolation_test.erl",
+      "lab3/gen_server/test/gen_server_interpolation_test.erl",
+      "lab3/gen_server/test/gen_server_parser_test.erl"
+    ],
 
-  %% Запуск тестов
-  TestModules = [proc_linear_interpolation_test,
-    proc_parser_test,
-    proc_points_generator_test,
-    proc_lagrange_interpolation_test,
-    gen_server_interpolation_test,
-    gen_server_parser_test],
+    %% Компилируем исходники
+    lists:foreach(fun(F) -> io:format("Компилируем ~s~n", [F]), c(F) end, Src),
 
-  lists:foreach(fun(M) ->
-    io:format("=== Running ~p ===~n", [M]),
-    eunit:test(M, [verbose])
-                end, TestModules),
+    %% Компилируем тесты
+    lists:foreach(fun(F) -> io:format("Компилируем тест ~s~n", [F]), c(F) end, Tests),
 
-  halt().
+    %% Запускаем тесты
+    AllTests = [list_to_atom(filename:basename(F, ".erl")) || F <- Tests],
+    lists:foreach(fun(M) -> io:format("\n=== Running ~p ===\n", [M]), eunit:test(M, [verbose]) end, AllTests).
